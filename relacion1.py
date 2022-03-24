1a8f698c0e357b61dc5ff79694e4c0fee4b30f75
Sad-Potato <e.sergiofern@go.ugr.es>
2022-03-24 18:24:31 +0100

    Ejercicio 5 terminado, faltan 6, 7 y 8, t-9 días

--- relacion1.py
+++ relacion1.py
@@ -3,9 +3,11 @@
     Relación 1: Aritmética modular
 
 """
+
 import numpy as np
 import os
 import random
+import math
 
 ######### Ejercicio 1 #########
 
@@ -96,7 +98,7 @@ def listaL_apuntes(a,u,s,p):
 
 
 # Función para determinar si dado un número p es (probablemente)
-# primo usando el método Miller-Rabin, si devolvemos que no es primo
+# primo usando el método de Miller-Rabin, si devolvemos que no es primo
 # lo hacemos con total seguridad y si decimos que lo es lo hacemos 
 # con un error de 1/4¹⁰ 
 
@@ -131,7 +133,103 @@ def esPrimo(p, k):
 
 ######### Ejercicio 5 #########
 
+# Para el cálculo de un s que sea mayor o igual que 
+# √p usamos el método de Newton-Raphson para el calculo
+# de raices cuadradas aunque sabemos la función 
+# math.sqrt de python tambien funciona para valores grandes
 
+# Valor n para el que queremos la raiz cuadrada y l
+# para el nivel de precisión
+def newtonRaphson(n, l):
+    # Primero hacemos una suposición con
+    # respecto al valor de la raiz cuadrada de n
+    raiz=n
+
+    # Condicion, iteramos mientras el error 
+    # entre iteraciones sea mayor a l
+    cond=True
+
+    while(cond):
+
+        raiz1=0.5*(raiz+(n/raiz))
+
+        # Si hemos llegado a una raiz con un error menor a 
+        # l paramos
+        if(abs(raiz1-raiz)<l):
+            cond=False
+        
+        # Actualizamos el valor de la raiz
+        # con el calculado a partir de este
+        raiz=raiz1
+
+    return raiz
+
+
+# Funcion para el algoritmo de paso enano-paso gigante
+# El algoritm tiene como entrada un a y c no nulos, un p 
+# primo y buscamos un b tal que a^b = c en Zₚ
+
+def pasoEnanoGigante(a,c,p):
+    
+    # El primer paso es encontrar un "s" que 
+    # sea mayor o igual que √p, lo calculamos 
+    # con una precisión suficiente
+    raizP=newtonRaphson(p,0.00000001)
+
+    # El valor de s sera el siguiente entero
+    # que exista a partir de la raiz de p
+    # (s es un entero mayor o igual que raiz de p)
+    s=math.ceil(raizP)
+    
+    # Calculamos la primera de las listas
+    listaS={}
+    elemento=c
+    
+    for r in np.arange(s): # Es decir [0,s-1]
+        # Añadimos el valor y el indice a la tabla hash
+        listaS[elemento]=r
+
+        # Calculamos el valor de la 
+        # siguiente iteración
+        elemento=modPotencia(elemento*a,1,p)
+        
+
+    # A partir de la primera lista vamos calculando 
+    # valores de la segunda hasta que encontremos 
+    # una coincidencia o lleguemos al final
+
+    # No necesitamos guardar los valores de la segunda lista
+    # ya que estamos comprobandolos 1 a 1
+
+    encontrado=False
+    elemento=inicial=modPotencia(a,s,p)
+    for t in np.arange(s)+1:
+
+        # Paramos si el elemento calculado en la iteración actual
+        # coincide con alguno de la primera lista
+        if elemento in listaS:
+            encontrado=True
+            logaritmo=t*s-listaS[elemento] # r
+            break
+
+        # Calculamos el valor de la 
+        # siguiente iteración
+        elemento=modPotencia(elemento*inicial,1,p)
+
+    # Si se ha encontrado alguna coincidencia entre las 2 listas
+    # se devuelve el algoritmo calculado, en otro caso se vuelve
+    # que no existe
+    if encontrado:
+        return logaritmo
+    
+    return "no existe el logaritmo"
+        
+
+
+
+
+    
+    
 
 
 
@@ -155,9 +253,12 @@ def main():
         tecla=input()
         #clear() # Limpiamos la terminal
 
-        a,b=28,13
+        
 
-        # Serie de "ifs" para cada uno de los ejercicios
+        #################################################################################
+        a,b=28,13
+        c,d=252336560693540533935881068298825202079,38942750026936412998460304986028600003
+
         if tecla=='1':
             m=mcd(a,b)
             print("MCD positivo de",a,"y "+str(b)+"="+str(m[0])+" ;u:",m[1],"v:",m[2])
@@ -165,8 +266,7 @@ def main():
             print(modInverso(a,b))
         if tecla=='3':
             # Probamos nuestra función con números muy grandes
-            print(modPotencia(252336560693540533935881068298825202079
-                ,38942750026936412998460304986028600003,623084000430982607975364879776457600049))
+            print(modPotencia(c,d,primos[-1]))
         if tecla=='4':
             # Probamos miller rabin para primos muy grandes
             print("Lista de números primos:")
@@ -177,7 +277,17 @@ def main():
                 print("\t",p,res)
         
         if tecla=='5':
-            print("work in progress")
+            a,c,p=13,2,19
+            print("El logaritmo en base",a,"de",c,"modulo",p,"es: ",
+                pasoEnanoGigante(a,c,p))
+
+        if tecla=='6':
+            print("Work in progress")
+
+
+
+
+        #################################################################################
 
         tecla=input("\nCualquier tecla para volver al menu, \"e\" para salir\n")
 
