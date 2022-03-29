d03b2e2413c59b37348469795b4e4c3e908af3a3
Sad-Potato <e.sergiofern@go.ugr.es>
2022-03-29 18:06:12 +0200

    7 la mitad hecho, 6 falta 0.75 por hacer

--- relacion1.py
+++ relacion1.py
@@ -4,10 +4,13 @@
 
 """
 
+from operator import mod
 import numpy as np
 import os
 import random
 import math
+
+from sympy import legendre_symbol
 
 ######### Ejercicio 1 #########
 
@@ -47,7 +50,7 @@ def modInverso(a,b):
 
 ######### Ejercicio 3 #########
 
-# Función para calcular a elevado a b modulo n
+# Función para el calculo de a^b modulo n
 def modPotencia(a,b,n):
     p=1
     while b>0:
@@ -224,13 +227,174 @@ def pasoEnanoGigante(a,c,p):
     
     return "no existe el logaritmo"
         
+######### Ejercicio 6 #########
+"""
+    Escribe una función que, dado un entero a y un primo p 
+    con a p = 1, devuelve r tal que r 2 ≡ a mód p
+    ([3, §2.3.4]; primero te hará falta implementar 
+    el símbolo de Jacobi [1, 2.149]).
+"""
 
+# Función para el cálculo del simbolo de Jacobi para 
+# un a sobre n (y para el cálculo del simbolo de Legendre en el 
+# caso de que n sea primo) (a/p(n))
 
+def simboloJacobiLegendre(a,n):
+    # Comprobamos que se cumplan las condiciones
+    # iniciales
+    """ assert n>=3 and a>=0 and a<n """
 
-
+    if a==0:
+        return 0
+    if a==1:
+        return 1
     
+    # Descomponemos a en potencias de 2 por un 
+    # número impar
+    e,a_1=descomposicionUyS(a)
+
+    # Si el número es par entonces s=1
+    s=0
+    if modPotencia(e,1,2)==0:
+        s=1
+    else: 
+        # Si no es impar comprobamos que n sea congruente con 1
+        # modulo 8 o que lo sea con 7 en cuyo caso s=1
+        if modPotencia(n-1,1,8)==0 or modPotencia(n-7,1,8)==0:
+            s=1
+        # Si no se cumplen las congruencias anteriores comprobamos que
+        # n sea congruente con 3 o 5 modulo 8
+        if modPotencia(n-3,1,8)==0 or modPotencia(n-5,1,8)==0:
+            s=-1
+    
+    # Si n y a_1 son congruentes con 3 módulo 4 entonces 
+    # s=-s
+    if modPotencia(n-3,1,4)==0 and modPotencia(a_1-3,1,4)==0:
+        s=-s
+    
+    n1=modPotencia(n,1,a_1)
+
+    if a_1==1:
+        return s
+    else:
+        return s*simboloJacobiLegendre(n1,a_1)
     
 
+# A partir de la función para el cálculo del
+# simbolo de Jacobi calculada procedemos con la 
+# primera de las funciones pedidas
+
+# La función tiene como entrada un entero a y
+# un primo p 
+def squaringTrapdoorRabin(a, p):
+
+    # Ejemplo de output para el simbolo de Jacobi para la 
+    # implementación propia y la implementación de sympy
+
+    """ print([legendre_symbol(i, 7) for i in range(7)])
+    print([simboloJacobiLegendre(i, 7) for i in range(7)]) """
+
+    # A partir de la implementación anterior del 
+    # símbolo de Jacobi tenemos como condición que el
+    # símbolo de Legendre(ya que si p es primo el simbolo de
+    # Jacobi es equivalente al de Legendre) para
+    # a sobre p sea 1, ya que sabemos
+    # que para que exista algún r tal que r^2 sea 
+    # congruente con a modulo p el símbolo de Legendre 
+    # para a y para p tiene que ser 1
+
+    if simboloJacobiLegendre(a,p)!=1:
+        return "No existe un r para el a y p dados"
+    
+    # Si existe un r lo buscamos
+
+    for m in np.arange(p-1)+1:
+        
+        if modPotencia(4*m+3,1,p)==0:
+            print("#### 1 ####")
+            print(a,m+1,p," #### ",modPotencia(-1,(p-1)/2,p))
+            return modPotencia(a,m+1,p)
+    
+        if modPotencia(4*m+1,1,p)==0:
+            
+            # Buscamos un valor aleatorio de b que
+            # satisfaga que su simbolo de Jacobi sobre
+            # p sea -1
+            b=random.randint(0,p-1)
+            while simboloJacobiLegendre(b,p)==-1:
+                b=random.randint(0,p-1)
+
+            i=modPotencia(2*m,1,p)
+            j=0
+
+            while modPotencia(i,1,2)!=0:
+                i=i/2
+                j=j/2
+                if modPotencia(modPotencia(a,i,p)*modPotencia(b,j,p),1,p)==-1:
+                    j=modPotencia(j+2*m,1,p)
+            
+            print("#### 2 ####")
+            return modPotencia(modPotencia(a,(i+1)/2,p)*modPotencia(b,j/2,p),1,p)
+   
+
+    """ for m in np.arange(p):
+        
+        if modPotencia(4*m+3,1,p)==0:
+            print("#### 1 ####")
+            return int(modPotencia(a,m+1,p))
+    for m in np.arange(p):
+        if modPotencia(8*m+5,1,p)==0 and modPotencia(modPotencia(a,2*m+1,p)-1,1,p)==0:
+            print("#### 2 ####")
+            return int(modPotencia(a,m+1,p))
+    for m in np.arange(p):
+        if modPotencia(8*m+5,1,p)==0 and modPotencia(modPotencia(a,2*m+1,p)+1,1,p)==0:
+            print("#### 3 ####")
+            return int(modPotencia(0.5*modPotencia(4*a,m+1,p)*(p+1),1,p))
+     """
+
+
+
+    """     u,s=descomposicionUyS(p-1)
+    r=modPotencia(a,(s+1)/2,p)
+
+    a_s=modPotencia(modPotencia(r,2,p)*modPotencia(a,p-1,p),1,p)
+
+    m=random.randint(0,p-1)
+    while simboloJacobiLegendre(m,p)!=-1:
+        m=random.randint(0,p-1)
+    
+    mu=modPotencia(m,s,p) """
+    
+######### Ejercicio 7 #########
+
+# Todo número natural mayor que 1, o es primo, o se puede expresar
+# de forma única como producto de primos
+
+# Factorización de un número por el método de 
+# Fermat, metodo útil en el caso de que n tenga dos 
+# divisores relativamente proximos y proximos a 
+# n/2
+def factorizacionFermat(n):
+
+    # Obtenemos la raiz cuadrada de n y obtenemos el entero
+    # inmediatamente superior, mientras x^2-n no sea un 
+    # cuadrado perfecto incrementamos x en una unidad
+    
+    x=math.ceil(newtonRaphson(n,0.000001))
+    while pow(round(newtonRaphson((pow(x,2)-n),0.00001)),2)!=(pow(x,2)-n):
+        # Compruebo que sea un cuadrado perfecto calculando la aproximacion
+        # de la raiz cuadrada y en el caso de que el cuadrado de esta apro-
+        # mación sea igual pues concluyo que lo es
+        x+=1
+    
+    # Una vez que tenemos un x tal que x^2-n es un cuadrado perfecto
+    # le asignamos a y el valor de la raiz cuadrada de x^2-n
+    y=round(newtonRaphson((pow(x,2)-n),0.00001))
+
+    return [x+y,x-y]
+    
+
+def factorizacionPollard(n):
 
 
 ###########################################################
@@ -242,22 +406,25 @@ def main():
     tecla=""
     random.seed(42)
 
-    # Lista de primos de gran tamaño para hacer 
-    # pruebas
-    primos=[46381, 768479, 9476407, 36780481, 562390847, 1894083629,
-    65398261921, 364879542899, 8590365927553, 28564333765949, 123456789101119,
-    623084000430982607975364879776457600049]
-
     while(tecla!="e"):
         print("Elija un ejercicio (1-8)")
         tecla=input()
         #clear() # Limpiamos la terminal
 
-        
-
         #################################################################################
+        # PARAMETROS PARA LOS EJERCICIOS
+        #################################################################################
+
         a,b=28,13
         c,d=252336560693540533935881068298825202079,38942750026936412998460304986028600003
+        e,f,g=13,2,19
+        # Lista de primos de gran tamaño para hacer 
+        # pruebas
+        primos=[46381, 768479, 9476407, 36780481, 562390847, 1894083629,
+        65398261921, 364879542899, 8590365927553, 28564333765949, 123456789101119,
+        623084000430982607975364879776457600049]
+
+        #################################################################################
 
         if tecla=='1':
             m=mcd(a,b)
@@ -277,14 +444,25 @@ def main():
                 print("\t",p,res)
         
         if tecla=='5':
-            a,c,p=13,2,19
-            print("El logaritmo en base",a,"de",c,"modulo",p,"es: ",
-                pasoEnanoGigante(a,c,p))
+            print("El logaritmo en base",e,"de",f,"modulo",g,"es: ",
+                pasoEnanoGigante(e,f,g))
 
         if tecla=='6':
-            print("Work in progress")
+            a_s=[1,3,4,5,9,12,14,15,16,20,23]
+            a_s=[4,6,7,9,10,11,13]
+            p=53
+            for a in a_s:
+                r=squaringTrapdoorRabin(a,p)
+                print(modPotencia(modPotencia(r,2,p)-a,1,p)," - ",modPotencia(r,2,p))
+        
+        if tecla=='7':
+            n=6352351
+            fact=factorizacionFermat(n)
+            print("La factorización del número",n,"por Fermat es",fact[0],"y",fact[1])
 
-
+            n2=7011461
+            fact2=factorizacionPollard(n2)
+            print("La factorización del número",n,"por Pollard es",fact2[0],"y",fact2[1])
 
 
         #################################################################################
